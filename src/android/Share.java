35e7a1019da30960ea69be7043081ceac88ed784
Trevor <trevor@pinkbike.com>
2018-12-29 02:27:16 -0800

    re-factor to use android FileProvider

--- src/android/Share.java
+++ src/android/Share.java
@@ -10,38 +10,45 @@ import org.json.JSONObject;
 import android.content.Intent;
 import android.net.Uri;
 
-    /**
-     * This class echoes a string called from JavaScript.
-     */
-    public class Share extends CordovaPlugin {
+import android.support.v4.content.FileProvider;
+import java.io.File;
+import android.content.Context;
 
-        @Override
-        public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-            if (action.equals("share")) {
-                String text = args.getString(0);
-                String title = args.getString(1);
-                String mimetype = args.getString(2);
-                this.share(text, title, mimetype, callbackContext);
-                return true;
-            }
-            return false;
+
+public class Share extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("share")) {
+            String text = args.getString(0);
+            String title = args.getString(1);
+            String mimetype = args.getString(2);
+            this.share(text, title, mimetype, callbackContext);
+            return true;
         }
+        return false;
+    }
 
-        private void share(String text, String title, String mimetype, CallbackContext callbackContext) {
-          try {
-            Intent sendIntent = new Intent();
+    private void share(String text, String title, String mimetype, CallbackContext callbackContext) {
+      try {
+        Intent sendIntent = new Intent();
+        if (mimetype.equals("text/plain")) {
             sendIntent.setAction(Intent.ACTION_SEND);
-            if (mimetype.equals("text/plain")) {
-                sendIntent.putExtra(Intent.EXTRA_TEXT, text);
-            } else {
-                sendIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse(text));
-            }
-            sendIntent.setType(mimetype);
-            cordova.getActivity().startActivity(Intent.createChooser(sendIntent, title));
-            callbackContext.success();
-            } catch(Error e) {
-                callbackContext.error(e.getMessage());
-            }
+            sendIntent.putExtra(Intent.EXTRA_TEXT, text);
+        } else {
+            sendIntent.setAction(Intent.ACTION_SEND);
+            Context context = this.cordova.getActivity().getApplicationContext();
+            Uri fileUri = FileProvider.getUriForFile(context, "com.pinkbike.trailforks.provider", new File(Uri.parse(text).getPath()));
+            sendIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            sendIntent.putExtra(Intent.EXTRA_STREAM, fileUri);
 
+            context.grantUriPermission("com.garmin.android.apps.connectmobile", fileUri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        }
+        sendIntent.setType(mimetype);
+        cordova.getActivity().startActivity(Intent.createChooser(sendIntent, title));
+        callbackContext.success();
+        } catch(Error e) {
+            callbackContext.error(e.getMessage());
         }
     }
+}
