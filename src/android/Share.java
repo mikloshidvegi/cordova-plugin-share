dcd64aec44b7c48ff56794676a1f80634c577ea4
Miklós Hidvégi <miklos.hidvegi@gmail.com>
2024-04-14 00:30:30 +0400

    Remove androidx.core.content.FileProvider import in Share.java

--- src/android/Share.java
+++ src/android/Share.java
@@ -10,7 +10,6 @@ import org.json.JSONObject;
 import android.content.Intent;
 import android.net.Uri;
 
-import androidx.core.content.FileProvider;
 import java.io.File;
 import android.content.Context;
 
